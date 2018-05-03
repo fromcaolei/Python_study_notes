f59a9ea042626aa718c02b847d73fde6228786e7
cl <649243905@qq.com>
2018-05-03 22:30:18 +0800

    .

--- Awesome-Python3-Webapp/www/orm.py
+++ Awesome-Python3-Webapp/www/orm.py
@@ -83,6 +83,8 @@ class Field(object):
     def __str__(self):  #用于将对象转化为适于人阅读的形式，可以使用str(obj)函数调用到该私有函数
         return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
 
+
+#定义多个相应的字段类型，调用父类方法的构造函数
 class StringField(Field):
 
     def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
@@ -108,36 +110,41 @@ class TextField(Field):
     def __init__(self, name=None, default=None):
         super().__init__(name, 'text', False, default)
 
+#定义一个元类，用于被model类继承，ORM框架的惯用手法
 class ModelMetaclass(type):
-
+    #__new__函数用来指导如何生成类的实例，比__init__更早的被调用，和__init__区别：__new__中我们通常只是修改dct，但是在__init__中，我们可以直接修改创建好的类
+    #第一个参数cls是将要创建的类，指向的是class；第二个参数是所定义类的名字，通常用类名.__name__获取；第三个是基类；第四个是dict类型，包含所定义类的属性或函数
     def __new__(cls, name, bases, attrs):
-        if name=='Model':
+        if name=='Model':  #如果所创建的类是Model类，不做处理
             return type.__new__(cls, name, bases, attrs)
-        tableName = attrs.get('__table__', None) or name
+
+        tableName = attrs.get('__table__', None) or name  #通过dict提供的get()方法确定key的存在，可返回指定结果，若不存在__table__键，则将所定义的类名当做表名保存
         logging.info('found model: %s (table: %s)' % (name, tableName))
-        mappings = dict()
-        fields = []
-        primaryKey = None
-        for k, v in attrs.items():
-            if isinstance(v, Field):
+
+        mappings = dict()  #创建一个字典类型的变量，保存所有"列属性"
+        fields = []  #创建一个list类型的变量，保存不是主键列的"列属性名"
+        primaryKey = None  #保存主键列属性的"列属性名"
+
+        for k, v in attrs.items():  #将所定义类的每个属性遍历，items()将dict对象转换成了包含tuple的list
+            if isinstance(v, Field):  #如果属性是个列名（field类在上方以声明过，用于存储列名和数据类型）
                 logging.info('  found mapping: %s ==> %s' % (k, v))
-                mappings[k] = v
-                if v.primary_key:
-                    # 找到主键:
-                    if primaryKey:
+                mappings[k] = v  #将所有"列属性"保存到字典变量中
+                if v.primary_key:  #如果这个列是主键列
+                    if primaryKey:  #若之前已经找到过一个主键列，则数据表异常
                         raise StandardError('Duplicate primary key for field: %s' % k)
-                    primaryKey = k
+                    primaryKey = k  #保存主键列"列属性名"到主键列变量中
                 else:
-                    fields.append(k)
+                    fields.append(k)  #将不是主键列的"列属性名"追加到list变量的末尾
         if not primaryKey:
             raise StandardError('Primary key not found.')
         for k in mappings.keys():
-            attrs.pop(k)
-        escaped_fields = list(map(lambda f: '`%s`' % f, fields))
-        attrs['__mappings__'] = mappings # 保存属性和列的映射关系
+            attrs.pop(k)  #删除所定义类的所有"列属性"元素
+        escaped_fields = list(map(lambda f: '`%s`' % f, fields))  #利用map将不是主键的列"列属性名"做成类似：['`k1`', '`k2`', '`k3`']
+        attrs['__mappings__'] = mappings  #给所定义类创建新私有属性__mappings__,保存属性和列的映射关系
         attrs['__table__'] = tableName
-        attrs['__primary_key__'] = primaryKey # 主键属性名
-        attrs['__fields__'] = fields # 除主键外的属性名
+        attrs['__primary_key__'] = primaryKey  #主键"列属性名"
+        attrs['__fields__'] = fields  #除主键外的"列属性名"
+        #以下四种方法保存了默认了增删改查操作,其中添加的反引号``,是为了避免与sql关键字冲突的,否则sql语句会执行出错
         attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
         attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
         attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
@@ -146,22 +153,22 @@ class ModelMetaclass(type):
 
 class Model(dict, metaclass=ModelMetaclass):
 
-    def __init__(self, **kw):
-        super(Model, self).__init__(**kw)
+    def __init__(self, **kw):若调用一个不存在的键，则弹出这个默认值
+        super(Model, self).__init__(**kw)  #继承了dict类，构造时也调用了这个父类的构造函数，就会在创建对象时，要求你输入字典形式的参数列表
 
-    def __getattr__(self, key):
+    def __getattr__(self, key):  #对调用到不存在的属性或函数时，会调用该函数，若该属性在类中已经是键值对，则该方法可通过下标[key]来取值（言简意赅的说，就是通过该函数实现了用点语法(".")获取字典中某键的值）
         try:
             return self[key]
         except KeyError:
             raise AttributeError(r"'Model' object has no attribute '%s'" % key)
 
-    def __setattr__(self, key, value):
+    def __setattr__(self, key, value):  #同上，通过该函数实现了用点语法(".")设置字典中某键的值
         self[key] = value
 
-    def getValue(self, key):
+    def getValue(self, key):  #通过下标[key]来取值的函数
         return getattr(self, key, None)
 
-    def getValueOrDefault(self, key):
+    def getValueOrDefault(self, key):  #若调用一个不存在的键，则弹出这个默认值
         value = getattr(self, key, None)
         if value is None:
             field = self.__mappings__[key]
@@ -171,9 +178,9 @@ class Model(dict, metaclass=ModelMetaclass):
                 setattr(self, key, value)
         return value
 
-    @classmethod
+    @classmethod  #这个修饰器可是这个函数调用可通过类名直接调用，而不需要生成对象去调用，即定义类的类方法
     @asyncio.coroutine
-    def findAll(cls, where=None, args=None, **kw):
+    def findAll(cls, where=None, args=None, **kw):  #第一个参数表示自身类
         ' find objects by where clause. '
         sql = [cls.__select__]
         if where:
@@ -196,8 +203,8 @@ class Model(dict, metaclass=ModelMetaclass):
                 args.extend(limit)
             else:
                 raise ValueError('Invalid limit value: %s' % str(limit))
-        rs = yield from select(' '.join(sql), args)
-        return [cls(**r) for r in rs]
+        rs = yield from select(' '.join(sql), args)  #一上代码配合类中原有的select语句，追加where及之后语句，时sql查询语句完整，异步调用select()函数获得结果多维tuple
+        return [cls(**r) for r in rs]  #！！！这个列表生成式表示没看懂cls(**r)的结果是什么？？？
 
     @classmethod
     @asyncio.coroutine
